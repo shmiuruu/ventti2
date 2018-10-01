c8ed69644e410fa5e6f29e2e6603fd2d19c4c47b
Artie <kaisaArtnait@gmail.com>
2018-10-01 13:20:22 +0300

    Hello World testing

--- JavaApplication12/src/javaapplication12/JavaApplication12.java
+++ JavaApplication12/src/javaapplication12/JavaApplication12.java
@@ -17,7 +17,7 @@ public class JavaApplication12 {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        
+        // Hello World :)
         //testi testi
     }
     
