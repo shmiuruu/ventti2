76bbdc75ff998b6cc6586cf2d2b16e60f68895af
helmi <helmi.kangastalo@gmail.com>
2018-10-01 13:13:05 +0300

    opetellaana

--- JavaApplication12/src/javaapplication12/JavaApplication12.java
+++ JavaApplication12/src/javaapplication12/JavaApplication12.java
@@ -16,6 +16,9 @@ public class JavaApplication12 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        
+        //testi testi
     }
     
 }
